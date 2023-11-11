d420a6f7ddd04dea7c494cc0527d2b1faaec5973
Jehee Cheon <jeheecheon@gmail.com>
2023-11-12 06:32:44 +0900

    Leave 구현

--- chat_server/src/MessageHandlers.cpp
+++ chat_server/src/MessageHandlers.cpp
@@ -322,7 +322,154 @@ void OnCsJoinRoom(int clientSock, const void* data) {
 }
 
 void OnCsLeaveRoom(int clientSock, const void* data) {
-    cout << "OnCsLeaveRoom" << endl;
+    // 해당 clientSock을 가지는 유저를 찾는다
+    shared_ptr<User> user;
+    if (!ChatServer::FindUserBySocketNum(clientSock, user)) {
+        cerr << "Failed to find the matching user with sock num....!!!!!";
+        return;
+    }
+
+    bool isUserInRoom = false;
+    const char* userName = nullptr;
+    string textForUserExiting;
+    string textForOtherUsers;
+
+    shared_ptr<Room> room = nullptr;
+
+    {
+        lock_guard<mutex> usersLock(ChatServer::usersMutex);
+        textForOtherUsers = user->GetUserName() + " 님이 퇴장했습니다.";
+
+        isUserInRoom = user->roomThisUserIn != nullptr;
+        if (isUserInRoom) {
+            lock_guard<mutex> roomsLock(ChatServer::roomsMutex);
+
+            room = user->roomThisUserIn; // 유저가 현재 속한 방 저장
+            textForUserExiting = "방제[" + room->title + "] 대화 방에서 퇴장했습니다.";
+
+            // 유저가 속한 방에서 유저를 찾는다
+            auto it = find(room->usersInThisRoom.begin(), room->usersInThisRoom.end(), user);
+            if (it != room->usersInThisRoom.end()) 
+                room->usersInThisRoom.erase(it);  // 유저를 방 내역에서 삭제
+            user->roomThisUserIn = nullptr; // 유저를 내보낸다
+
+            // 채팅방에 아무도 없다면 채팅방을 삭제
+            if (room->usersInThisRoom.size() == 0)
+                ChatServer::rooms.erase(room);
+        }
+    }
+
+    char* msgToSendForUserExiting = nullptr; // 보낼 메시지
+    short bytesToSendForUserExiting; // 보낼 데이터의 바이트 수
+    char* msgToSendForOtherUsers = nullptr; // 보낼 메시지
+    short bytesToSendForOtherUsers; // 보낼 데이터의 바이트 수
+
+    // json 포맷
+    if (ChatServer::isJson) {
+        string jsonStringForUserExiting;
+        string jsonStringForOtherUsers;
+
+        if (!isUserInRoom) {
+            // RapidJSON document 생성
+            rapidjson::Document jsonDocForUserExiting;
+            jsonDocForUserExiting.SetObject();
+
+            // "type" : "SCSystemMessage" 추가
+            rapidjson::Value typeValueForUserExiting;
+            typeValueForUserExiting.SetString("SCSystemMessage", jsonDocForUserExiting.GetAllocator());
+            jsonDocForUserExiting.AddMember("type", typeValueForUserExiting, jsonDocForUserExiting.GetAllocator());
+
+            // "text" : + 시스템 메시지 추가
+            rapidjson::Value textValueForUserExiting;
+            textValueForUserExiting.SetString(textForUserExiting.c_str(), jsonDocForUserExiting.GetAllocator());
+            jsonDocForUserExiting.AddMember("text", textValueForUserExiting, jsonDocForUserExiting.GetAllocator());
+
+            // document 를 json string으로 변환
+            rapidjson::StringBuffer bufferForUserExiting;
+            rapidjson::Writer<rapidjson::StringBuffer> writerForUserExiting(bufferForUserExiting);
+            jsonDocForUserExiting.Accept(writerForUserExiting);
+            jsonStringForUserExiting = bufferForUserExiting.GetString();
+        } else {
+            // RapidJSON document 생성
+            rapidjson::Document jsonDocForUserExiting;
+            jsonDocForUserExiting.SetObject();
+
+            // "type" : "SCSystemMessage" 추가
+            rapidjson::Value typeValueForUserExiting;
+            typeValueForUserExiting.SetString("SCSystemMessage", jsonDocForUserExiting.GetAllocator());
+            jsonDocForUserExiting.AddMember("type", typeValueForUserExiting, jsonDocForUserExiting.GetAllocator());
+
+            // "text" : + 시스템 메시지 추가
+            rapidjson::Value textValueForUserExiting;
+            textValueForUserExiting.SetString(textForUserExiting.c_str(), jsonDocForUserExiting.GetAllocator());
+            jsonDocForUserExiting.AddMember("text", textValueForUserExiting, jsonDocForUserExiting.GetAllocator());
+
+            // document 를 json string으로 변환
+            rapidjson::StringBuffer bufferForUserExiting;
+            rapidjson::Writer<rapidjson::StringBuffer> writerForUserExiting(bufferForUserExiting);
+            jsonDocForUserExiting.Accept(writerForUserExiting);
+            jsonStringForUserExiting = bufferForUserExiting.GetString();
+
+            // 새로운 JSON 문자열을 만들기 위해 jsonDoc 초기화
+            rapidjson::Document jsonDocForOtherUsers;
+            jsonDocForOtherUsers.SetObject();
+
+            // "type" : "SCSystemMessage" 추가
+            rapidjson::Value typeValueForOtherUsers;
+            typeValueForOtherUsers.SetString("SCSystemMessage", jsonDocForOtherUsers.GetAllocator());
+            jsonDocForOtherUsers.AddMember("type", typeValueForOtherUsers, jsonDocForOtherUsers.GetAllocator());
+
+            // "text" : + 시스템 메시지 추가
+            rapidjson::Value textValueForOtherUsers;
+            textValueForOtherUsers.SetString(textForOtherUsers.c_str(), jsonDocForOtherUsers.GetAllocator());
+            jsonDocForOtherUsers.AddMember("text", textValueForOtherUsers, jsonDocForOtherUsers.GetAllocator());
+
+            // document 를 json string으로 변환
+            rapidjson::StringBuffer bufferForOtherUsers;
+            rapidjson::Writer<rapidjson::StringBuffer> writerForOtherUsers(bufferForOtherUsers);
+            jsonDocForOtherUsers.Accept(writerForOtherUsers);
+            jsonStringForOtherUsers = bufferForOtherUsers.GetString();
+
+            // json 앞에 json의 바이트수 추가하여 보낼 데이터 생성
+            short msgBytesInBigEnndian = htons(jsonStringForOtherUsers.length());
+            bytesToSendForOtherUsers = jsonStringForOtherUsers.length() + 2;
+            msgToSendForOtherUsers = new char[bytesToSendForOtherUsers];
+            memcpy(msgToSendForOtherUsers, &msgBytesInBigEnndian, 2);
+            memcpy(msgToSendForOtherUsers + 2, jsonStringForOtherUsers.c_str(), jsonStringForOtherUsers.length());
+        }
+
+        // json 앞에 json의 바이트수 추가하여 보낼 데이터 생성
+        short msgBytesInBigEnndian = htons(jsonStringForUserExiting.length());
+        bytesToSendForUserExiting = jsonStringForUserExiting.length() + 2;
+        msgToSendForUserExiting = new char[bytesToSendForUserExiting];
+        memcpy(msgToSendForUserExiting, &msgBytesInBigEnndian, 2);
+        memcpy(msgToSendForUserExiting + 2, jsonStringForUserExiting.c_str(), jsonStringForUserExiting.length());
+    }
+
+    // protobuf 포맷
+    else {
+
+    }
+
+    if (msgToSendForUserExiting != nullptr) {
+        ChatServer::CustomSend(user->socketNumber, msgToSendForUserExiting, bytesToSendForUserExiting);
+
+        if (isUserInRoom) {
+            {
+                lock_guard<mutex> usersLock(ChatServer::usersMutex);
+                {
+                    lock_guard<mutex> roomsLock(ChatServer::roomsMutex);
+                    if (room != nullptr) 
+                        for (auto u : room->usersInThisRoom) 
+                            ChatServer::CustomSend(u->socketNumber, msgToSendForOtherUsers, bytesToSendForOtherUsers);
+                }
+            }
+
+            delete[] msgToSendForOtherUsers;
+        }
+            
+        delete[] msgToSendForUserExiting;
+    }
 }
 
 void OnCsChat(int clientSock, const void* data) {
